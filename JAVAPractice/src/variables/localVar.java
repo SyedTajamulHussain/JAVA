18b9c84609154e950567d17afb70f8e0c05be502
SyedTajamulHussain <tajamulsyed26@gmail.com>
2024-11-01 20:29:09 +0530

    Local variable example

---
+++ JAVAPractice/src/variables/localVar.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class localVar {
+    public static void main(String[] args) {
+        localVariable();
+
+    }
+    public static void localVariable(){
+        int a = 5;
+        System.out.println(a);
+    }
+
+    public static void localVariable2(){
+        //System.out.println(a);
+
+// A variable declared inside the body of the method is called local variable. You can use this variable
+// only within that method and the other methods in the class aren't even aware that the variable exists.
+        //local variable cannot be static
+    }
+}
