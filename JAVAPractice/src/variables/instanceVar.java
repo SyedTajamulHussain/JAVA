d3cd262de0f2762f158000f90467384ae74bac09
SyedTajamulHussain <tajamulsyed26@gmail.com>
2024-11-15 20:08:14 +0530

    Local and instance variable example

--- JAVAPractice/src/variables/instanceVar.java
+++ JAVAPractice/src/variables/instanceVar.java
@@ -1,7 +1,27 @@
 package variables;
 
+//A variable declared inside the class but outside the body of the method, is called an instance variable.
+// It is not declared as static.
+//An instance variable is a variable that is specific to a certain object.
+// It is declared within the curly braces of the class but outside of any method.
+// The value of an instance variable can be changed by any method in the class,
+// but it is not accessible from outside the class.
 public class instanceVar {
-    public static void main(String[] args) {
+    int id;
+    String name;
 
+    public instanceVar(){
+        this.id = 1;
+        this.name= "john";
+        // Default Constructor
+        // initializing Instance Variable
     }
+
+    public static void main(String[] args) {
+        instanceVar ins =new instanceVar();
+        System.out.println(ins.name);
+        System.out.println(ins.id);
+    }
+
+
 }
--- JAVAPractice/src/variables/staticVar.java
+++ JAVAPractice/src/variables/staticVar.java
@@ -1,4 +1,15 @@
 package variables;
 
+
+//A variable that is declared as static is called a static variable. It cannot be local.
+// You can create a single copy of the static variable and share it among all the instances of the class.
+// Memory allocation for static variables happens only once when the class is loaded in the memory
 public class staticVar {
+static int a =90;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
 }
+
+
